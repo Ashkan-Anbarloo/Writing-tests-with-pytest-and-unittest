8ccb6fae22e27d592f2d8bab2a5834725661f5e1
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-08-31 13:05:53 +0330

    test Request Queue Manager part2

--- Request_Queue_Manager/test_request_queue_manager.py
+++ Request_Queue_Manager/test_request_queue_manager.py
@@ -17,3 +17,29 @@ class TestRequestQueue(unittest.TestCase):
             self.queue.add_request('    ')
         with self.assertRaises(ValueError):
             self.queue.add_request(7423)
+
+    def test_process_request(self):
+        self.queue.add_request('First')
+        self.queue.add_request('Secend')
+        processed = self.queue.process_next()
+        self.assertEqual(processed , 'First')
+        self.assertEqual(self.queue.peek_next() , 'Secend')
+
+
+    def test_process_empty_queue(self):
+        with self.assertRaises(IndexError):
+            self.queue.process_next()
+
+    def test_peek_next_empty(self):
+        self.assertIsNone(self.queue.peek_next())
+
+    def test_get_all_request(self):
+        self.queue.add_request("A")
+        self.queue.add_request("B")
+        self.assertEqual(self.queue.get_all_requests() , ['A','B'])
+
+    def test_is_empty_and_count(self):
+        self.assertTrue(self.queue.is_empty())
+        self.queue.add_request("somethings")
+        self.assertFalse(self.queue.is_empty())
+        self.assertEqual(self.queue.count() , 1)
