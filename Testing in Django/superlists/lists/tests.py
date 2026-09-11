5ec3666afaa0ed98ed21a35d54d000246fc12d44
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-09-12 00:31:58 +0330

    Refactor homepage view to use a template

--- Testing in Django/superlists/lists/tests.py
+++ Testing in Django/superlists/lists/tests.py
@@ -19,6 +19,11 @@ class HomePageTest(TestCase):
     
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        self.assertContains(response , '<title>To-Do lists</title>')
-        self.assertContains(response, "<html>")
-        self.assertContains(response, "</html>")
+        # self.assertContains(response , '<title>To-Do lists</title>')
+        # self.assertContains(response, "<html>")
+        # self.assertContains(response, "</html>")
+        self.assertTemplateUsed(response , 'home.html')
+    
+    def test_render_homepage_conyent(self):
+        response = self.client.get('/')
+        self.assertContains(response , 'To-Do')
--- Testing in Django/test_1.py
+++ Testing in Django/test_1.py
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_todo_list(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To Do' , self.browser.title)
+        self.assertIn('To-Do' , self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME , 'h1').text
         self.assertIn('To-Do' , header_text)
         inputbox = self.browser.find_element(By.ID , 'id_new_item')
@@ -26,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID , 'id_list_table')
         rows = table.find_elements(By.TAG_NAME , 'tr')
         self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
-        
+
         self.fail('Finish the test!')
         # [...rest of comments as before]
 if __name__ == '__main__':
