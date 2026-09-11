fa8c6895b907191d17141bfd51d89e45ab20daa4
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-09-11 12:12:09 +0330

    First unit test and view function

--- Testing in Django/superlists/lists/tests.py
+++ Testing in Django/superlists/lists/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1 , 2)
+# class SmokeTest(TestCase):
+#     def test_bad_math(self):
+#         self.assertEqual(1+1 , 2)
+
+class HomePageTest(TestCase):
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>To-Do lists</title>' , html)
+        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.endswith('</html>'))
