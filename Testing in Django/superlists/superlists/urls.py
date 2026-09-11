0c66bebe571b24fdb8486ff7e5542bf51f9b7247
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-09-11 12:43:24 +0330

    url config, map / to home_page view

--- Testing in Django/superlists/superlists/urls.py
+++ Testing in Django/superlists/superlists/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 # from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.urls import path
+from lists.views import home_page
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path("", home_page, name="home"),
 ]
