5bfd9be5840b742e0b732853aaa5cd74122882bb
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-09-11 19:56:01 +0330

    Functional test now checks we can input a to-do item

--- Testing in Django/test_1.py
+++ Testing in Django/test_1.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -9,13 +12,25 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_todo_list(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To Do' , self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME , 'h1').text
+        self.assertIn('To-Do' , header_text)
+        inputbox = self.browser.find_element(By.ID , 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder') , 'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID , 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME , 'tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        
         self.fail('Finish the test!')
         # [...rest of comments as before]
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    unittest.main()
 
 
 
