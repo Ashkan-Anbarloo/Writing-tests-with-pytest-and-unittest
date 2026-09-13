f660f37ede9c75ec4e36d4a09abb3a751b946de2
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-09-13 13:45:18 +0330

    Improving Error Messages in Tests

--- Testing in Django/superlists/lists/tests.py
+++ Testing in Django/superlists/lists/tests.py
@@ -24,6 +24,16 @@ class HomePageTest(TestCase):
         # self.assertContains(response, "</html>")
         self.assertTemplateUsed(response , 'home.html')
     
-    def test_render_homepage_conyent(self):
+    # def test_render_homepage_conyent(self):
+    #     response = self.client.get('/')
+    #     self.assertContains(response , 'To-Do')
+
+    def test_renders_input_form(self):
         response = self.client.get('/')
-        self.assertContains(response , 'To-Do')
+        self.assertContains(response , "<form method='POST'>")
+        self.assertContains(response , 'name="item_text"')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/' , data={"item_text":"A new list item"})
+        self.assertContains(response , "A new list item")
+        self.assertTemplateUsed(response , 'home.html')
