64c9e6a9685fa0e95bff0504b0dd3226a99b9e5e
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-09-19 13:01:33 +0330

    make functional_tests an app, use LiveServerTestCase

---
+++ Testing in Django/superlists/functional_tests/tests.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+from django.test import LiveServerTestCase
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    # def test_can_start_a_todo_list(self):
+    #     self.browser.get(self.live_server_url)
+
+    def check_for_row_in_list_table(self , row_text):
+        table = self.browser.find_element(By.ID , "id_list_table")
+        rows = table.find_elements(By.TAG_NAME , 'tr')
+        self.assertIn(row_text , [row.text for row in rows])
+
+    def test_can_start_a_todo_list(self):
+        # self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
+        self.assertIn('To-Do' , self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME , 'h1').text
+        self.assertIn('To-Do' , header_text)
+
+        inputbox = self.browser.find_element(By.ID , 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder') , 'Enter a to-do item')
+
+        inputbox.send_keys('Buy peacock feathers')
+        # inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+        # table = self.browser.find_element(By.ID , 'id_list_table')
+        # rows = table.find_elements(By.TAG_NAME , 'tr')
+
+        # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),f"New to-do item did not appear in table. Contents were:\n{table.text}",)
+        # self.assertIn(
+        # "2: Use peacock feathers to make a fly",
+        # [row.text for row in rows],
+        # )
+        # self.assertIn(
+        # "1: Buy peacock feathers",
+        # [row.text for row in rows],
+        # )
+        #-----------
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        
+        #-----------
+        # self.fail('Finish the test!')
+        # [...rest of comments as before]
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+# browser = webdriver.Firefox()
+# browser.get('http://localhost:8000')
+
+# assert 'To-Do' in browser.title 
+# browser.quit()
