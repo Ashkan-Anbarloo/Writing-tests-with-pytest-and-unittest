4010e87bfb8e60a90383447b817bd8caaa5281f1
Ashkan-Anbarloo <ashkanbyo@gmail.com>
2026-09-24 10:49:08 +0330

    chapter 7

--- Testing in Django/superlists/lists/tests.py
+++ Testing in Django/superlists/lists/tests.py
@@ -31,7 +31,7 @@ class HomePageTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get('/')
-        self.assertContains(response , "<form method='POST' action='/'>")
+        self.assertContains(response , "<form method='POST' action='/lists/new'>")
         self.assertContains(response , 'name="item_text"')
     
     # def test_display_all_list_items(self):
@@ -60,9 +60,23 @@ class HomePageTest(TestCase):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
     
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count() , 0)
+    # def test_only_saves_items_when_necessary(self):
+    #     self.client.get('/')
+    #     self.assertEqual(Item.objects.count() , 0)
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new' , data={'item_text' : 'A new list item'})
+        self.assertEqual(Item.objects.count() , 1)
+        new_item = Item.objects.get()
+        self.assertEqual(new_item.text , 'A new list item')
+    
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new' , data={'item_text' : 'A new list item'})
+        self.assertRedirects(response , '/lists/the-only-list-in-the-world/')
+        
+
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -72,7 +86,7 @@ class ListViewTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertContains(response , "<form method='POST' action='/'>")
+        self.assertContains(response , "<form method='POST' action='/lists/new'>")
         # self.assertContains(response , '<input name="item_text">')
         self.assertContains(response , 'name="item_text"')
 
--- Testing in Django/superlists/lists/views.py
+++ Testing in Django/superlists/lists/views.py
@@ -5,23 +5,16 @@ from lists.models import Item
 
 # home_page = None
 def home_page(request):
-    # return HttpResponse("<html><title>To-Do lists</title></html>")
-    # return render(request , 'home.html')
-    # if request.method == 'POST':
-    #     return HttpResponse('You submitted' + request.POST["item_text"])
-    # return render(request , 'home.html')
-    if request.method == "POST":
-        # item = Item()
-        # item.text = request.POST.get('item_text' , "")
-        # item.text = request.POST['item_text']
-        # item.save()
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-    # items = Item.objects.all()
-    # return render(request , 'home.html' , {'new_item_text':request.POST.get("item_text" , "")})
+    # if request.method == "POST":
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request , 'list.html' , {'items':items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
